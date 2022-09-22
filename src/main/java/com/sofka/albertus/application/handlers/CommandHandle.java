c647131a97ac78ee9e1389feb34af1ebc1cc4a85
Luisa Fernanda Barragan Avila <luisaavila304@gmail.com>
2022-09-22 17:52:52 -0500

    Configuración con Db mongo funcional mas POST createBlockChain

--- src/main/java/com/sofka/albertus/application/handlers/CommandHandle.java
+++ src/main/java/com/sofka/albertus/application/handlers/CommandHandle.java
@@ -1,45 +1,38 @@
 package com.sofka.albertus.application.handlers;
 
 
+import co.com.sofka.domain.generic.DomainEvent;
 import com.sofka.albertus.business.usecases.CreateBlockChainUseCase;
-
-import com.sofka.albertus.business.usecases.CreateBlockUseCase;
-import com.sofka.albertus.domain.commands.CreateBlock;
 import com.sofka.albertus.domain.commands.CreateBlockChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 
-import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class CommandHandle {
 
 
   @Bean
-  public RouterFunction<ServerResponse> createBlockChain(CreateBlockChainUseCase usecase) {
-    return RouterFunctions.route(
-        RequestPredicates.POST("/create/blockChain")
-            .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
-        request -> usecase.apply(request.bodyToMono(CreateBlockChain.class))
-            .then(ServerResponse.ok().build())
-
-    );
-  }
-
-  @Bean
-  public RouterFunction<ServerResponse> createBlock(CreateBlockUseCase usecase) {
-    return RouterFunctions.route(
-        RequestPredicates.POST("/create/block")
-            .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
-        request -> usecase.apply(request.bodyToMono(CreateBlock.class))
-            .then(ServerResponse.ok().build())
-
+  public RouterFunction<ServerResponse> create(CreateBlockChainUseCase useCase) {
+    return route(
+        POST("/create/block").and(accept(MediaType.APPLICATION_JSON)),
+        request -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromPublisher(
+                useCase.apply(request.bodyToMono(CreateBlockChain.class)),
+                DomainEvent.class))
     );
   }
 
 
 }
+
+
+
